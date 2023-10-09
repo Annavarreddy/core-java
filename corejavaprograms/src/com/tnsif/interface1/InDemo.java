e32b7ee0c267fb9e5600446eefed2e3926053f94
annav <annav@annavaramreddy.bbrouter>
2023-10-09 14:58:18 +0530

    TENTH COMMIT

---
+++ corejavaprograms/src/com/tnsif/interface1/InDemo.java
@@ -0,0 +1,21 @@
+package com.tnsif.interface1;
+
+public  class InDemo implements ChildInterface,Interfaceone {
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		System.out.println("print method");
+		
+	}
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("show method");
+	}
+	
+	
+	
+	}
+
