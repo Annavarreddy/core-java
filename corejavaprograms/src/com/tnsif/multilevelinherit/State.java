fb6f625c75c4d5a8430f68b23e4543fa7b4723d2
annav <annav@10.0.0.103>
2023-10-09 09:00:30 +0530

    FOURTH COMMIT

---
+++ corejavaprograms/src/com/tnsif/multilevelinherit/State.java
@@ -0,0 +1,13 @@
+package com.tnsif.multilevelinherit;
+
+public class State extends Country  {
+	private String Statename;
+
+	public String getStatename() {
+		return Statename;
+	}
+
+	public void setStatename(String statename) {
+		Statename = statename;
+	}
+}
