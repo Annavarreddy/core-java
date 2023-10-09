31269f323280dfa6383fa74e596ee70c58e57fc3
annav <annav@10.0.0.103>
2023-10-09 09:10:43 +0530

    FIFTH COMMIT

---
+++ corejavaprograms/src/com/tnsif/singleinherit/SingleDemo.java
@@ -0,0 +1,12 @@
+package com.tnsif.singleinherit;
+
+public class SingleDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student st = new Student("Annavaram",47013476,"Rajamundry",9948547);
+		System.out.println(st);
+	}
+	}
+
+
