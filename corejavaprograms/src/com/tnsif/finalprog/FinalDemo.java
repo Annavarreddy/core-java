42a30cb863da955e2a61f39ace723f86725ee451
annav <annav@10.0.0.101>
2023-10-09 15:28:56 +0530

    FOURTEEN COMMIT

---
+++ corejavaprograms/src/com/tnsif/finalprog/FinalDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.finalprog;
+
+public class FinalDemo {
+	int a=80;
+    void display() {
+   	 System.out.println("the value of a is" +a);
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     FinalDemo fd=new FinalDemo();
+     fd.display();
+	
+	}
+}
