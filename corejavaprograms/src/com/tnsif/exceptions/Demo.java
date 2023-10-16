c5d32fd8140a22aff44bfa22493560a0dd7f3237
annav <annav@192.168.1.37>
2023-10-16 16:59:16 +0530

    NINEENTEEN COMMIT

---
+++ corejavaprograms/src/com/tnsif/exceptions/Demo.java
@@ -0,0 +1,13 @@
+package com.tnsif.exceptions;
+import java.util.Scanner;
+public class Demo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          Scanner in = new Scanner(System.in);
+          int rollno=in.nextInt();
+          if(rollno<0)
+          {
+        	  throw new ArithmeticException("roll no cannot be zero");
+          }
+	}
+}
